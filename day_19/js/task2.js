d0701a9c57103fb0084d74e084db1725b461cd55
M Nabeel P <71066857+n48331@users.noreply.github.com>
2022-07-20 11:54:35 +0530

    18-21

---
+++ day_19/js/task2.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        $('div').animate({
+            height:'300px',
+            width:'500px',
+            margin:'50px',
+            
+        })
+    })
+})
